73b4e50a3f543ab5f11cc64dc1aba435d0f56fa0
grhncnrbs <gurhan.cinarbas@hotmail.com>
2024-01-02 22:30:39 +0300

    .

--- src/test/java/com/grhncnrbs/usermanagementapp/UserManagementAppApplicationTests.java
+++ src/test/java/com/grhncnrbs/usermanagementapp/UserManagementAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserManagementAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
